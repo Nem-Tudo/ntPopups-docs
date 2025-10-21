100729e2bd53e311220fd6cdd09706a69fbe9211
Nem Tudo <gf98289@gmail.com>
2025-10-21 17:07:08 -0300

    update

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -11,13 +11,14 @@ export default defineConfig({
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Live examples', link: 'https://ntpopups.nemtudo.me' },
     ],
 
     sidebar: sidebar,
     socialLinks: [
       { icon: 'github', link: 'https://github.com/Nem-Tudo/ntPopups' },
+      { icon: 'discord', link: 'https://discord.gg/nemtudo' },
+      { icon: 'instagram', link: 'https://instagram.com/_nemtudo_' },
     ]
   }
 })
